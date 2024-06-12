8a64860095a7ea33a5f81551698da920687cc637
George <geoedmo@gmail.com>
2024-06-12 11:11:47 -0400

    Added Interface class for Highlighting

---
+++ Source/Aura/Public/Interaction/EnemyInteraction.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EnemyInteraction.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UEnemyInteraction : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class AURA_API IEnemyInteraction
+{
+	GENERATED_BODY()
+
+
+public:
+
+	virtual void HighlightActor() = 0;
+	virtual void UnhighlightActor() = 0;
+
+};
